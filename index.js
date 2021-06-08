b25db3bf66e5a5eec8a46084918edaaada399c0b
Brian Young <berkshirehunt@gmail.com>
2021-06-08 11:01:14 -0700

    image manipulations

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index.ejs')
 })
 
 app.post('/', upload.single('myFile'), (req, res) => {
   cloudinary.uploader.upload(req.file.path, (result) => {
     var imageId = `${result.public_id}.jpg`
     // res.send(result)
-    let src = cloudinary.image(imageId, {
-      effect: 'grayscale',
-      width: 300,
-      x: 0,
-      crop: 'crop',
+    let src = cloudinary.image('x6ebsjdkdjfx6qzolopp.jpg', {
+      transformation: [
+        { effect: 'oil_paint:30', radius: 16, width: 310, x: 0, crop: 'crop' },
+        { angle: 0 },
+      ],
     })
-    res.render('image', { imgSrc: src })
+    res.render('image.ejs', { imgSrc: src })
   })
 })
 
